35a7039a28c95346196e2e5b47a23a95ef4c1998
Chetan Goel <chetan.goel2019@gmail.com>
2024-12-05 18:04:36 -0500

    added login page and database connection

---
+++ init.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+app = Flask(__name__)
+app.secret_key = 'secret key'
+
+conn = pymysql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    port = 8889,
+    db = 'donationDB',
+    charset = 'utf8mb4',
+    cursorclass = pymysql.cursors.DictCursor
+)
+
+@app.route('/test')
+def health():
+    return 'connected!'
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+
+    cursor = conn.cursor()
+
+    query = 'SELECT * FROM person WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+
+    data = cursor.fetchone()
+
+    cursor.close()
+    if (data):
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        error = 'Invalid login'
+        return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 5000, debug=True)
+
